6796f350e6195a298638de914dff7226acdf9d24
David THIBAU <david.thibau@gmail.com>
2020-05-20 11:39:49 +0200

    Ajout des tests

--- src/main/java/org/formation/model/Livraison.java
+++ src/main/java/org/formation/model/Livraison.java
@@ -12,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Livraison {
 
@@ -26,6 +28,7 @@ public class Livraison {
 	private Status status;
 	
 	@OneToMany(cascade = CascadeType.ALL)
+	@JsonIgnore
 	private List<Trace> historique = new ArrayList<>();
 
 	private Instant creationDate;
@@ -80,6 +83,28 @@ public class Livraison {
 
 	public void setCreationDate(Instant creationDate) {
 		this.creationDate = creationDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livraison other = (Livraison) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 
